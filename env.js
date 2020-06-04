64745ee56f9c489d47a7c0c5f84a8c7c52a18726
FishPoolCloud <github@fishpool.cloud>
2020-06-04 20:44:15 +0800

    Update to 1.3

---
+++ env.js
@@ -0,0 +1,14 @@
+window.settings = {
+  // 站点标题
+  title: '云上鱼池',
+  // API
+  host: 'https://api.fishpool.cloud',
+  // 主题
+  theme: {
+    sidebar: 'light',
+    header: 'dark',
+    color: 'default'
+  },
+  // 背景
+  background_url: ''
+}
